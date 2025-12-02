6bf9485ce632727bbdce617fcae277f7e589974f
Jaad Mohammed <134211721+uijaad@users.noreply.github.com>
2025-12-02 14:06:59 +0200

    Add files via upload

---
+++ perfect_numbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter First Number : ");
+        int n1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter Second Number : ");
+        int n2 = int.Parse(Console.ReadLine());
+        Console.Write("\n");
+        for (int i = n1; i <= n2; i++)
+        {
+            int sum = 0;
+            for (int j = 1; j <= i / 2; j++)
+            {
+                if (i % j == 0)
+                {
+                    sum += j;
+                }
+            }
+            if (sum == i)
+            {
+                Console.WriteLine(i + " Is Perfect Number");
+            }
+        }
+        Console.Write("\n");
+    }
+}
