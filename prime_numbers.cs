f7a5b2a879aee52df61c9fa602823d202679e365
Jaad Mohammed <134211721+uijaad@users.noreply.github.com>
2025-12-02 13:39:01 +0200

    Add files via upload

---
+++ prime_numbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter First Number : ");
+        int n1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter Second Number : ");
+        int n2 = int.Parse(Console.ReadLine());
+        for (int x = n1; x <= n2; x++)
+        {
+            bool isPrime = true;
+            if (x < 2)
+            {
+                isPrime = false;
+            }
+            else
+            {
+                for (int i = 2; i * i <= x; i++)
+                {
+                    if (x % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine("Prime Number : " + x);
+            }
+        }
+    }
+    
+}
